bf3242ecf4204bbdbf57759c4f96e19d37829e0e
brenpintelos <ec2-user@ip-172-31-93-145.ec2.internal>
2022-08-11 21:06:09 +0000

    fixing sintax

--- myec2/main.tf
+++ myec2/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-	region = "us-east-1"
-	access_key = ""
-	secret_key = ""
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
 
-resource "aws_instance" "ec2" {
-	ami = "ami-090fa75af13c156b4"
-	instance type = "t2.micro"
-}
+  resource "aws_instance" "ec2" {
+    ami           = "ami-090fa75af13c156b4"
+    instance_type = "t2.micro"
+  }
 }
